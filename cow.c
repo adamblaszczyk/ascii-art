d23e70de1277f8e5302a95a13eb943246d4e455a
Adam Błaszczyk <blaszczyk.adam@gmail.com>
2024-09-12 14:25:57 +0200

    Add files via upload

---
+++ cow.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("\n");
+    printf("            ^__^\n");
+    printf("            (oo)\\_______\n");
+    printf("            (__)\\       )\\/\\\n");
+    printf("                ||----w |\n");
+    printf("                ||     ||\n");
+    printf("\n");
+    return 0;
+}
---
+++ cow.py
@@ -0,0 +1,12 @@
+def rysuj_krowe():
+    krowa = r'''
+            ^__^
+            (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+    '''
+    print(krowa)
+
+rysuj_krowe()
+input()
